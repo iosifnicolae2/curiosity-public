3874073631dcfb5a5de2eb755bd151e255fa9175
Nicolae Iosif <iosif@mailo.dev>
2020-02-18 23:39:23 +0200

    Add more instructions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,7 @@
 # Please run this script on bash
 
 apt install python3-pip
-apt-get install python3-setuptools
-apt-get install python-setuptools
-pip install -U pip setuptools
+pip3 install -U pip setuptools
 
 pip3 install virtualenv
 
@@ -12,3 +10,8 @@ virtualenv venv
 source venv/bin/activate
 
 pip install .
+
+# conda create -n conda_venv python=3.7.3 anaconda
+# conda activate conda_venv
+# pip install setuptools
+# python -m pip install .
